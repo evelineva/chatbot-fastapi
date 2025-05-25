aa72bbca77e714d0a86d605f9f92264bccc263a6
evelineva <eveline.anthony@student.matanauniversity.ac.id>
2025-05-25 11:41:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app.add_middleware(
     allow_headers=["*"],  # Allow all headers
 )
 
-RASA_URL = "https://8509-111-94-130-190.ngrok-free.app/webhooks/rest/webhook"
+RASA_URL = "https://26c3-111-94-130-190.ngrok-free.app/webhooks/rest/webhook"
 # 🎯 Model untuk request body
 class UserMessage(BaseModel):
     message: str
